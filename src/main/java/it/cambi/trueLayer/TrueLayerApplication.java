aab56cc99a926c148afca8c29d55a16d5bba7a2b
lucacambi@rentalcars.com <lucacambi@rentalcars.com>
2020-11-14 12:19:45 +0000

    adds to git repo

---
+++ src/main/java/it/cambi/trueLayer/TrueLayerApplication.java
@@ -0,0 +1,13 @@
+package it.cambi.trueLayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrueLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrueLayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/cambi/trueLayer/TrueLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.cambi.trueLayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrueLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
